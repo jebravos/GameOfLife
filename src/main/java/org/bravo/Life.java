6ac30fb7c5da4158de0452682f2d48ee54adfcb3
Esteban Bravo <jorgeesteban.bravosanchez@external.engie.com>
2022-12-09 18:56:27 +0100

    [R]: Illegal argument exception when invalid seed

--- src/main/java/org/bravo/Life.java
+++ src/main/java/org/bravo/Life.java
@@ -7,46 +7,52 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-public class Life extends CellsWrapper{
+public class Life extends CellsWrapper {
 
 
     private Map<Integer, List<Cell>> mapOfCells;
 
     public Life(Seed seed) {
+
+
+
         mapOfCells = seed.toMap();
     }
 
-    public void tick_() {
-
-        Map<Integer, List<Cell>> newGenerationMapOfCells = new LinkedHashMap();
-
-        this.mapOfCells.keySet().stream()
-                .sorted(Comparator.naturalOrder())
-                .forEach(y -> {
-                    this.mapOfCells.get(y)
-                            .stream()
-                            .forEach(cell -> {
-
-                                List<Cell> rowCells = newGenerationMapOfCells.computeIfAbsent(y, k -> new LinkedList<>());
-                                newGenerationMapOfCells.get(y).add(newGenerationFromCell(cell));
-
-                            });
-
-                });
-
-        mapOfCells = newGenerationMapOfCells;
-    }
     public void tick() {
         tick_();
     }
 
-    private Cell newGenerationFromCell(Cell cell) {
+    private void tick_() {
 
-        if (findCell(cell).get().isAlive()) {
-            return newGenerationFromAliveCell(cell);
-        } else {
-            return newGenerationFromDeadCell(cell);
+        Map<Integer, List<Cell>> newGenerationMapOfCells = new LinkedHashMap<>();
+
+        this.mapOfCells.keySet()
+                .forEach(y -> this.mapOfCells.get(y)
+                        .forEach(cell -> newGenerationFromCell(cell)
+                                .ifPresent(c -> newGenerationMapOfCells.computeIfAbsent(y, k -> new LinkedList<>())
+                                        .add(c)
+                                )
+                        )
+                );
+
+        mapOfCells = newGenerationMapOfCells;
+    }
+
+    private Optional<Cell> newGenerationFromCell(Cell cell) {
+
+        final Optional<Cell> cell1 = findCell(cell);
+
+        if (cell1.isEmpty()) {
+            return cell1;
         }
+
+        if (cell1.get().isAlive()) {
+            return Optional.ofNullable(newGenerationFromAliveCell(cell));
+        } else {
+            return Optional.ofNullable(newGenerationFromDeadCell(cell));
+        }
+
     }
 
     private Cell newGenerationFromDeadCell(Cell cell) {
@@ -68,13 +74,6 @@ public class Life extends CellsWrapper{
         }
     }
 
-    private long countAliveNeighbors(Cell cell) {
-        return getNeighbors(cell)
-                .stream()
-                .filter(Cell::isAlive)
-                .count();
-    }
-
 
     @Override
     public Optional<Cell> findCell(final Coordinates coordinates) {
@@ -87,7 +86,7 @@ public class Life extends CellsWrapper{
 
 
     public Optional<Cell> findCell(int x, int y) {
-        return findCell(new Coordinates(x,y));
+        return findCell(new Coordinates(x, y));
     }
 
 
--- src/test/java/org/bravo/LifeTests.java
+++ src/test/java/org/bravo/LifeTests.java
@@ -2,84 +2,110 @@ package org.bravo;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import java.util.stream.Stream;
 
 import static org.bravo.Cell.alive;
 import static org.bravo.Cell.dead;
 
 class LifeTests {
 
+    public static final Cell[][] EMPTY_CELLS = {};
+    public static final Seed EMPTY_SEED = new Seed(EMPTY_CELLS);
     private Cell[][] seed = {
-            { alive(new Coordinates(0,0)), dead(new Coordinates(1,0)),  alive(new Coordinates(2,0)) }, // 0 1 2 x
-            { alive(new Coordinates(0,1)), dead(new Coordinates(1,1)),  dead(new Coordinates(2,1)) },  // 1
-            { dead(new Coordinates(0,2)),  alive(new Coordinates(1,2)), alive(new Coordinates(2,2)) }  // 2
-                                         // y
+            {alive(new Coordinates(0, 0)), dead(new Coordinates(1, 0)), alive(new Coordinates(2, 0))}, // 0 1 2 x
+            {alive(new Coordinates(0, 1)), dead(new Coordinates(1, 1)), dead(new Coordinates(2, 1))},  // 1
+            {dead(new Coordinates(0, 2)), alive(new Coordinates(1, 2)), alive(new Coordinates(2, 2))}  // 2
+            // y
     };
 
-    @Test
-    @DisplayName("Any live cell with two or three live neighbours survives.")
-    void t0(){
-        // Given seed
-        Life life = new Life(new Seed(seed));
-        // When
-        life.print();
-        System.out.println("------------------------------");
-        life.tick();
-        life.print();
-        System.out.println("------------------------------");
-        // then
-        Assertions.assertTrue(life.findCell(0,1).isPresent());
-        Assertions.assertTrue(life.findCell(0,1).get().isAlive());
 
-        Assertions.assertTrue(life.findCell(1,2).isPresent());
-        Assertions.assertTrue(life.findCell(1,2).get().isAlive());
+    @Nested
+    class SuccessfulTests {
+        @Test
+        @DisplayName("Any live cell with two or three live neighbours survives.")
+        void t0() {
+            // Given seed
+            Life life = new Life(new Seed(seed));
+            // When
+            life.print();
+            System.out.println("------------------------------");
+            life.tick();
+            life.print();
+            System.out.println("------------------------------");
+            // then
+            Assertions.assertTrue(life.findCell(0, 1).isPresent());
+            Assertions.assertTrue(life.findCell(0, 1).get().isAlive());
+
+            Assertions.assertTrue(life.findCell(1, 2).isPresent());
+            Assertions.assertTrue(life.findCell(1, 2).get().isAlive());
+
+        }
+
+        @Test
+        @DisplayName("Any dead cell with three live neighbours becomes a live cell.")
+        public void t1() {
+            // Given seed
+            Life life = new Life(new Seed(seed));
+            // When
+            life.print();
+            System.out.println("------------------------------");
+            life.tick();
+            life.print();
+            System.out.println("------------------------------");
+            // then
+            Assertions.assertTrue(life.findCell(1, 0).isPresent());
+            Assertions.assertTrue(life.findCell(1, 0).get().isAlive());
+
+            Assertions.assertTrue(life.findCell(2, 1).isPresent());
+            Assertions.assertTrue(life.findCell(2, 1).get().isAlive());
+        }
+
+        @Test
+        @DisplayName("All other live cells die in the next generation. Similarly, all other dead cells stay dead.")
+        public void t2() {
+            // Given seed
+            Life life = new Life(new Seed(seed));
+            // When
+            life.print();
+            System.out.println("------------------------------");
+            life.tick();
+            life.print();
+            System.out.println("------------------------------");
+            // then
+            Assertions.assertTrue(life.findCell(0, 0).isPresent());
+            Assertions.assertTrue(life.findCell(0, 0).get().isDead());
+
+            Assertions.assertTrue(life.findCell(0, 2).isPresent());
+            Assertions.assertTrue(life.findCell(0, 2).get().isDead());
+
+            Assertions.assertTrue(life.findCell(1, 1).isPresent());
+            Assertions.assertTrue(life.findCell(1, 1).get().isDead());
+
+            Assertions.assertTrue(life.findCell(2, 0).isPresent());
+            Assertions.assertTrue(life.findCell(2, 0).get().isDead());
+
+            Assertions.assertTrue(life.findCell(2, 2).isPresent());
+            Assertions.assertTrue(life.findCell(2, 2).get().isDead());
+        }
 
     }
 
-    @Test
-    @DisplayName("Any dead cell with three live neighbours becomes a live cell.")
-    public void t1(){
-        // Given seed
-        Life life = new Life(new Seed(seed));
-        // When
-        life.print();
-        System.out.println("------------------------------");
-        life.tick();
-        life.print();
-        System.out.println("------------------------------");
-        // then
-        Assertions.assertTrue(life.findCell(1,0).isPresent());
-        Assertions.assertTrue(life.findCell(1,0).get().isAlive());
+    @Nested
+    class ValidationTests {
 
-        Assertions.assertTrue(life.findCell(2,1).isPresent());
-        Assertions.assertTrue(life.findCell(2,1).get().isAlive());
-    }
-    @Test
-    @DisplayName("All other live cells die in the next generation. Similarly, all other dead cells stay dead.")
-    public void t2(){
-        // Given seed
-        Life life = new Life(new Seed(seed));
-        // When
-        life.print();
-        System.out.println("------------------------------");
-        life.tick();
-        life.print();
-        System.out.println("------------------------------");
-        // then
-        Assertions.assertTrue(life.findCell(0,0).isPresent());
-        Assertions.assertTrue(life.findCell(0,0).get().isDead());
+        @Test
+        @DisplayName("Illegal argument exception when invalid seed")
+        void t0() {
 
-        Assertions.assertTrue(life.findCell(0,2).isPresent());
-        Assertions.assertTrue(life.findCell(0,2).get().isDead());
+            Stream.of(null, EMPTY_SEED)
+                    .forEach(seed -> Assertions.assertThrows(IllegalArgumentException.class, () -> new Life(null)));
 
-        Assertions.assertTrue(life.findCell(1,1).isPresent());
-        Assertions.assertTrue(life.findCell(1,1).get().isDead());
 
-        Assertions.assertTrue(life.findCell(2,0).isPresent());
-        Assertions.assertTrue(life.findCell(2,0).get().isDead());
+        }
 
-        Assertions.assertTrue(life.findCell(2,2).isPresent());
-        Assertions.assertTrue(life.findCell(2,2).get().isDead());
     }
 
 }
